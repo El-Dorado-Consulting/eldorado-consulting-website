b3ea9ab8511333951f7835c56411fbd461b45e4a
tkreek <twkreek@gmail.com>
2021-07-02 15:15:13 -0700

    I wonder if this fixed anything

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-static';
+import netlify from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-        adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+        adapter: netlify()
 
 	},
 

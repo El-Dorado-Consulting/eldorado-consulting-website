ad3046100a50533446f4c7b8e843776c22b20bdc
tkreek <twkreek@gmail.com>
2021-07-03 14:17:08 -0700

    fixed blog post

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,31 +1,36 @@
 <script context="module">
   export async function load({ page, fetch }) {
     const slug = page.params.slug;
-    const post = await fetch(`./${slug}.json`)
-        .then((r) => r.json());
+    const post = await fetch(`./${slug}.json`).then((r) => r.json());
     return {
-      props: { post }
+      props: { post },
     };
   }
 </script>
 
 <script>
   export let post;
-  let date = post.metadata.date.toUpperCase();
+  import Header from '$lib/components/Header.svelte'
 </script>
 
 <svelte:head>
   <title>{post.metadata.title}</title>
 </svelte:head>
-
-<h1 class="text-3xl mb-4">{post.metadata.title}</h1>
-<h2 class="text-xl mb-4">{post.metadata.subtitle}</h2>
-<h2 class="text-sm mb-4">{post.metadata.date}</h2>
-
-
-<div class="prose">
-  {@html post.content}
+<Header/>
+<div class="w-full mt-36">
+    <div class="header">
+      <h1 class="text-3xl mb-4 self-start">{post.metadata.title}</h1>
+      <h2 class="text-xl mb-4">{post.metadata.subtitle}</h2>
+      <h2 class="text-sm mb-4">{post.metadata.date}</h2>
+    </div>
+    <div class="prose header">
+      {@html post.content}
+    </div>
 </div>
 
-<style >
+<style>
+  .header {
+    max-width: 635px;
+    margin: 0 auto;
+  }
 </style>

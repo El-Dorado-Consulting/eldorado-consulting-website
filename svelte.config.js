ee8c610218dab419c13dae13208bef89fa050ced
tkreek <twkreek@gmail.com>
2021-09-17 14:04:05 -0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-static';
 import netlify from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */

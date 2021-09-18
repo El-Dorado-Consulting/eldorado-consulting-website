94ec5b8fb3cda7077c05214fe13aed25fb077196
tkreek <twkreek@gmail.com>
2021-09-17 19:11:42 -0700

    updated svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,27 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import preprocess from "svelte-preprocess";
+import netlify from '@sveltejs/adapter-netlify'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    "extensions": [".svelte", ...mdsvexConfig.extensions],
+    kit: {
+		target: '#svelte',
+        adapter: netlify(),
+        paths: {
+            base: '',
+            assets: ''
+          },
+
+	},
+    preprocess: [preprocess({
+        "postcss": true
+    }), mdsvex(mdsvexConfig)]
 };
 
 export default config;
+// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+process.env.TAILWIND_MODE = dev ? "watch" : "build";

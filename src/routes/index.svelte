2a51fcfb5ff260e93221c8cdc23ed5f5a7b28ef8
tkreek <twkreek@gmail.com>
2021-09-18 19:51:44 -0700

    index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,19 @@
-
 <script context="module">
-	const allPosts = import.meta.glob("./blog/*md")
-	let body = []
+	const allPosts = import.meta.glob("./blog/*md");
+	let body = [];
 	for (let path in allPosts) {
 		body.push(
-			allPosts[path]().then(({metadata}) => {
-				return {path, metadata}
+			allPosts[path]().then(({ metadata }) => {
+				return { path, metadata };
 			})
-		)
+		);
 	}
-	export const load = async() => {
-		const posts = await Promise.all(body)
+	export const load = async () => {
+		const posts = await Promise.all(body);
 		return {
-			props:{posts}
-		}
-	}
-
+			props: { posts },
+		};
+	};
 </script>
 
 <script>
@@ -28,68 +26,68 @@
 	import Blog from "$lib/components/Blog.svelte";
 	import Teams from "$lib/components/Teams.svelte";
 	import Contact from "$lib/components/Contact.svelte";
+	import Us from "$lib/components/Us.svelte";
+	import Testimonial from "$lib/components/testimonial.svelte";
+	import Footer from "$lib/components/Footer.svelte";
 
-	export let posts
+	export let posts;
 	let parallax;
-	
-	function scrollHandler (event) {
-		console.log(event.detail.section)
-		parallax.scrollTo(event.detail.section)
-	}
-	
-</script>
 
+	function scrollHandler(event) {
+		console.log(event.detail.section);
+		parallax.scrollTo(event.detail.section);
+	}
+</script>
 
 <Header on:message={scrollHandler} />
 <div class="wrapper">
-<Parallax
-	bind:this={parallax}
-	sections={6}
-	config={{ stiffness: 1, damping: 1 }}
->
-	<ParallaxLayer rate={0.2} span={1.5} offset={0}>
-		<section class="section bg-img bg1-lg" />
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={0}>
-		<section class="section">
-			<Banner on:message={scrollHandler} />
-		</section>
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={1}>
-		<section class="section bg-mint">
-			<About />
-		</section>
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={2}>
-		<section class="section bg-white">
-			<Services />
-		</section>
-	</ParallaxLayer>
-	<ParallaxLayer rate={0.2} span={1.5} offset={3}>
-		<section class="section bg2-lg" />
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={3}>
-		<section class="section">
-			<Blog blogs={posts} />
-		</section>
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={4}>
-		<section  class="section bg-white">
-			<Teams />
-		</section>
-	</ParallaxLayer>
-	<ParallaxLayer rate={0} span={1} offset={5}>
-		<section class="section bg-white">
-			<Contact />
-		</section>
-	</ParallaxLayer>
-</Parallax>
+	<Parallax
+		bind:this={parallax}
+		sections={2}
+		config={{ stiffness: 1, damping: 1 }}
+	>
+		<ParallaxLayer rate={0.2} span={1} offset={0}>
+			<section class="section bg-img bg1-lg" />
+		</ParallaxLayer>
+		<ParallaxLayer rate={0} span={1} offset={0}>
+			<section class="section">
+				<Banner on:message={scrollHandler} />
+			</section>
+		</ParallaxLayer>
+		<ParallaxLayer rate={0} span={1} offset={1}>
+			<section class="section bg-mint">
+				<About />
+			</section>
+		</ParallaxLayer>
+	</Parallax>
+	<section class="section bg-gray">
+		<Us />
+	</section>
+
+	<section class="section bg-white">
+		<Services />
+	</section>
+	<section class="section bg2-lg" />
+	<section class="section">
+		<Blog blogs={posts} />
+	</section>
+	<section class="section bg-white">
+		<Teams />
+	</section>
+	<section class="section bg-gray">
+		<Testimonial />
+	</section>
+	<section class="section bg-white">
+		<Contact />
+	</section>
+	<section>
+		<Footer />
+	</section>
 </div>
 
 <style>
-
-	.wrapper{
-		max-width: 1536px;
+	.wrapper {
+		max-width: 1800px;
 		margin: 0 auto;
 	}
 	.section {
@@ -99,6 +97,8 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		padding-top: 2rem;
+		padding-bottom: 2rem;
 	}
 	.bg-img {
 		background-size: 100%;
@@ -112,5 +112,4 @@
 		background-image: url("https://i.ibb.co/yp1Bp1d/bg2-lg.png");
 		opacity: 60%;
 	}
-
 </style>

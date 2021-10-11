1b9b9c9e7ddec42a16ec9eb25c83f0ae59e81af9
tkreek <twkreek@gmail.com>
2021-10-11 08:23:50 -0700

    commit recovery

--- src/lib/components/Us.svelte
+++ src/lib/components/Us.svelte
@@ -5,21 +5,16 @@
   let thomas = `${assets}/thomas.png`
 </script>
 
-
-
 <div class=" w-full flex flex-row h-full"> 
     <div class = "flex flex-col items-center w-full">
       <img class="rounded-full w-5/12" src={erik} alt="">
       <p class="mt-4 font-bold">Erik </p>
     </div>
-    <div class = "flex flex-col items-center w-full">
       <img class="rounded-full w-5/12" src={sarah} alt="">
       <p class="mt-4 font-bold">Sarah </p>
     </div>
     <div class = "flex flex-col items-center w-full">
       <img class="rounded-full w-5/12" src={thomas} alt="">
       <p class="mt-4 font-bold">Thomas</p>
-    </div>
-
-  </div>
+</div>
 

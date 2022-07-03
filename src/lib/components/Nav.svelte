638574c6e01810ac6799c0af488d4b98464ec1a5
tkreek <twkreek@gmail.com>
2022-07-02 21:58:06 -0700

    tweak blog

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { slide } from 'svelte/transition';
+  import { slide } from "svelte/transition";
 
   let jobDescription =
     "https://half-tax-a1e.notion.site/Fragile-Software-Engineer-a929c9b1d292418b91eb4d0fb1b5fa54";
@@ -48,44 +48,39 @@
         /></svg
       >
     </button>
-    <div class="hidden sm:flex justify-between items-center text-md" >
+    <div class="hidden sm:flex justify-between items-center text-md">
       <div>
         <ul class="flex pt-2 space-x-4">
           <li>
             <a href="/blog" class="px-4 py-transition-colors">Blog</a>
           </li>
-
+          <li />
           <li>
-            <a
-              href="/contact"
-              class="px-3 py-1.5 bg-copper-400 text-white rounded-md hover:bg-copper-500 transition-colors ml-2"
-              >Contact Us</a
-            >
+            <a href="/Contact" class="px-4 py-transition-colors">Contact</a>
           </li>
         </ul>
       </div>
     </div>
   </div>
-{#if visible}
-  <div
-    id="mobile-menu"
-    class="block absolute z-50 white mt-4 w-screen right-1 bg-white shadow"
-    transition:slide
-  >
-    <ul >
-      <!---
+  {#if visible}
+    <div
+      id="mobile-menu"
+      class="block absolute z-50 white mt-4 w-screen right-1 bg-white shadow"
+      transition:slide
+    >
+      <ul>
+        <!---
       <li class="py-4 px-4 text-md border-b hover:bg-gray-50" >
         <a class="inline-block w-full h-full" href="/">About</a>
       </li>
     -->
-      <li class="py-4 px-4 text-md border-b hover:bg-gray-50" >
-        <a class="inline-block w-full h-full" href="/blog">Blog</a>
-      </li>
-      <li class="py-4 px-4 text-md border-b hover:bg-gray-50" >
-        <a class="inline-block w-full h-full" href="/blog">Contact</a>
-      </li>
-    </ul>
-  </div>
+        <li class="py-4 px-4 text-md border-b hover:bg-gray-50">
+          <a class="inline-block w-full h-full" href="/blog">Blog</a>
+        </li>
+        <li class="py-4 px-4 text-md border-b hover:bg-gray-50">
+          <a class="inline-block w-full h-full" href="/blog">Contact</a>
+        </li>
+      </ul>
+    </div>
   {/if}
 </nav>
-

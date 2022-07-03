adfe530efa925c334af6e943343f9e5136a3b1ff
tkreek <twkreek@gmail.com>
2022-07-02 21:32:41 -0700

    footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<div class="bg-purple-900 text-center w-full" style="margin-bottom: -10px;"> 
-  <p class="py-10 text-purple-50">© fragile, Inc | San Francisco, CA | 2022</p>
+<div class="bg-graphite text-center w-full" style="margin-bottom: -10px;"> 
+  <p class="py-10 text-purple-50">© El Dorado Consulting, Inc | San Francisco, CA | 2022</p>
 </div>
